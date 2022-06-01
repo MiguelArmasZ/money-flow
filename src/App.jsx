04386a05fb4e01da052e0cb7a4572b48a05165cf
Miguel Armas <miguelalbertoaz@gmail.com>
2022-06-01 17:31:21 +0200

    agregado hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { DataProvider } from './DataContext'
 import { Routing } from './routes/Routing'
 
 export const App = () => {
-    return (
-        <DataProvider>
-            <BrowserRouter>
-                <Routing />
-            </BrowserRouter>
-        </DataProvider>
-    )
+  return (
+    <DataProvider>
+      <HashRouter>
+        <Routing />
+      </HashRouter>
+    </DataProvider>
+  )
 }
--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -14,22 +14,20 @@ import { NotFound } from '../pages/NotFound'
 
 export const Routing = () => {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='' element={<Home />} />
-          <Route path='registrar' element={<FormDaily />} />
-          <Route path='vision-general' element={<Overview />} />
-          <Route path='ingresos' element={<Records />} />
-          <Route path='gastos' element={<Records />} />
-          <Route path='ahorro' element={<Saving />} />
-          <Route path='categorias' element={<Categories />} />
-          <Route path='add-categoria' element={<FormCategory />} />
-          <Route path='historial' element={<History />} />
-          <Route path='reiniciar' element={<Reset />} />
-        </Route>
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home />} />
+        <Route path='registrar' element={<FormDaily />} />
+        <Route path='vision-general' element={<Overview />} />
+        <Route path='ingresos' element={<Records />} />
+        <Route path='gastos' element={<Records />} />
+        <Route path='ahorro' element={<Saving />} />
+        <Route path='categorias' element={<Categories />} />
+        <Route path='add-categoria' element={<FormCategory />} />
+        <Route path='historial' element={<History />} />
+        <Route path='reiniciar' element={<Reset />} />
+      </Route>
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
